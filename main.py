04c5752b8931b2d897f1040075df079d33b43ac1
George Miranda <george92miranda@gmail.com>
2017-08-17 13:42:15 -0700

    test video

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import picamera
+from subprocess import call
+from time import sleep
 
 print "Accessing Camera"
 with picamera.PiCamera() as camera:
-    camera.resolution(1280, 720)
-    camera.capture("./image.jpg")
+    camera.resolution = (1280, 720)
+    camera.start_recording("./sampleVideo.h264")
+    sleep(10)
+    camera.stop_recording()
 
-print "Picture Taken"
+print "Video being converted"
+command1 = "MP4Box --add sampleVideo.h264 " + "converted.mp4"
+command2 = "sudo rm sampleVideo.h264"
+call([command1, command2], shell=True)
+
+print "File finished"
